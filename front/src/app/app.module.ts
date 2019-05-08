0768bbcb38904943debc9886009a305c9517ab4e
bashenov98 <bashenov98@gmail.com>
2019-05-08 21:41:42 +0600

    third

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProviderService } from './services/provider.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ProviderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
